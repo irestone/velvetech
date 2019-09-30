7639a85c657f89b21de882926aabf41c8528cb1e
irestone <irestone.work@gmail.com>
2019-09-30 16:00:15 +0300

    Update category creation form: MUI, cancelable

--- app/view/src/components/forms/AddCategory.js
+++ app/view/src/components/forms/AddCategory.js
@@ -3,75 +3,83 @@ import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-
 import { addCategory } from '../../store/actions/data/categories'
-import {
-  showCreateCategoryForm,
-  hideCreateCategoryForm,
-} from '../../store/actions/views/categories'
+
+import { Button, withStyles } from '@material-ui/core'
+import { TextField } from './muiFields'
 
 // component
 class AddCategoryComponent extends Component {
-  onSubmit(data) {
+  onSubmit(values) {
     const { addCategory, reset } = this.props
-    addCategory(data)
+    addCategory(values)
     reset()
   }
 
+  onCancel(values) {
+    const { reset, cancel } = this.props
+    reset()
+    cancel()
+  }
+
   render() {
-    const {
-      isAddCategoryFormHidden,
-      showCreateCategoryForm,
-      hideCreateCategoryForm,
-    } = this.props
+    const { classes, handleSubmit, pristine } = this.props
     return (
-      <div className='add-category'>
-        <button
-          onClick={
-            isAddCategoryFormHidden
-              ? showCreateCategoryForm
-              : hideCreateCategoryForm
-          }
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          name='name'
+          component={TextField}
+          type='text'
+          placeholder='Name'
+          autoFocus
+          className={classes.field}
+        />
+        <Button
+          type='submit'
+          color='primary'
+          className={classes.button}
+          disabled={pristine}
         >
-          New Category
-        </button>
-        {!isAddCategoryFormHidden && (
-          <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
-            <Field
-              name='name'
-              component='input'
-              type='text'
-              placeholder='Name'
-            />
-            <button>Submit</button>
-          </form>
-        )}
-      </div>
+          Create
+        </Button>
+        <Button
+          className={classes.button}
+          onClick={handleSubmit(this.onCancel.bind(this))}
+        >
+          Cancel
+        </Button>
+      </form>
     )
   }
 }
 
 AddCategoryComponent.propTypes = {
-  isAddCategoryFormHidden: PropTypes.bool,
-  showCreateCategoryForm: PropTypes.func,
-  hideCreateCategoryForm: PropTypes.func,
   addCategory: PropTypes.func,
+  cancel: PropTypes.func,
+  pristine: PropTypes.bool,
   handleSubmit: PropTypes.func,
   reset: PropTypes.func,
+  classes: PropTypes.object,
 }
 
+// styling
+const styles = ({ spacing }) => ({
+  button: {
+    marginLeft: spacing(2),
+  },
+  field: {
+    width: spacing(42),
+  },
+})
+
+const Styled = withStyles(styles)(AddCategoryComponent)
+
 // form fields sync
-const AddCategoryReduxFormWrapper = reduxForm({ form: 'AddCategory' })(
-  AddCategoryComponent
-)
+const SyncedFields = reduxForm({ form: 'AddCategory' })(Styled)
 
 // store connection
 // todo: optimize selection
 export const AddCategory = connect(
-  ({
-    views: {
-      categories: { isAddCategoryFormHidden },
-    },
-  }) => ({ isAddCategoryFormHidden }),
-  { addCategory, showCreateCategoryForm, hideCreateCategoryForm }
-)(AddCategoryReduxFormWrapper)
+  () => ({}),
+  { addCategory }
+)(SyncedFields)
--- app/view/src/views/Categories.js
+++ app/view/src/views/Categories.js
@@ -1,16 +1,64 @@
 import React from 'react'
 
+// state
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import {
+  showCreateCategoryForm,
+  hideCreateCategoryForm,
+} from '../store/actions/views/categories'
+
 // sub-components
 import { AddCategory } from '../components/forms/AddCategory'
 import { CategoryList } from '../components/CategoryList'
 
 // ui
-import { Paper } from '@material-ui/core'
+import { Typography, makeStyles, Button, Box } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(3),
+  },
+  creatingForm: {
+    marginTop: theme.spacing(3),
+  },
+}))
 
 // component
-export const Categories = () => (
-  <Paper className='Categories'>
-    <AddCategory />
-    <CategoryList />
-  </Paper>
-)
+export const CategoriesComponent = ({
+  isAddCategoryFormHidden,
+  showCreateCategoryForm,
+  hideCreateCategoryForm,
+}) => {
+  const styles = useStyles()
+  return (
+    <Box className={styles.root}>
+      <Typography variant='h3'>Categories</Typography>
+      <div className={styles.creatingForm}>
+        {isAddCategoryFormHidden ? (
+          <Button color='primary' onClick={showCreateCategoryForm}>
+            New category
+          </Button>
+        ) : (
+          <AddCategory cancel={hideCreateCategoryForm} />
+        )}
+      </div>
+      <CategoryList />
+    </Box>
+  )
+}
+
+CategoriesComponent.propTypes = {
+  isAddCategoryFormHidden: PropTypes.bool,
+  showCreateCategoryForm: PropTypes.func,
+  hideCreateCategoryForm: PropTypes.func,
+}
+
+export const Categories = connect(
+  ({
+    views: {
+      categories: { isAddCategoryFormHidden },
+    },
+  }) => ({ isAddCategoryFormHidden }),
+  { showCreateCategoryForm, hideCreateCategoryForm }
+)(CategoriesComponent)
