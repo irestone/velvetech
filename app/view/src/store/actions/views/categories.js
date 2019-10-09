aec8a17cfac758d1f5c31aa83acaebf5ebd68d7c
irestone <irestone.work@gmail.com>
2019-10-09 11:45:23 +0300

    Refactor: typo

--- app/view/src/store/actions/views/categories.js
+++ app/view/src/store/actions/views/categories.js
@@ -2,7 +2,7 @@ import { createAction } from '../../../utils/actions'
 
 const ca = createAction.bind(this, 'views/categories')
 
-export const showAddCategoryForm = ca('showw', 'add_category_form')
+export const showAddCategoryForm = ca('show', 'add_category_form')
 export const hideAddCategoryForm = ca('hide', 'add_category_form')
 export const editCategory = ca('edit', 'category')
 export const cancelCategoryEditing = ca('cancel', 'category_editing')
